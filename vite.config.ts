a7df45c2a4f69a7af2f25329918062978bdeaf0e
Hannah Noelia <58449528+hannahNchan@users.noreply.github.com>
2025-01-28 21:30:10 -0600

    changing vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from 'vite'  
+import vue from '@vitejs/plugin-vue'  
+import { fileURLToPath } from 'node:url'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig({  
+  plugins: [
+    vue(),
+  ],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "/src")
-    },
-  },
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      },
+    ]
+  }
 })
