6441ec66c5909c610c896d369b8b0f878b29d9c1
Hannah Noelia <58449528+hannahNchan@users.noreply.github.com>
2025-01-29 16:38:42 -0600

    Added data manager for AWS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import AppProvidersWrapper from "./components/wrappers/AppProvidersWrapper"
 import configureFakeBackend from "./helpers/fake-backend"
 import AppRouter from "./routes/router"
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
 
 import '@/assets/scss/style.scss'
 
@@ -13,5 +15,7 @@ function App() {
     </AppProvidersWrapper>
   )
 }
+
+Amplify.configure(outputs);
 
 export default App
